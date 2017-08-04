6c36ceb1df12064eb7be908602513eec3e233203
vishvAsaH <vishvas.vasuki@gmail.com>
2017-08-04 16:42:29 -0700

    Rebuilt.

--- docs/polymer-app/service-worker.js
+++
@@ -1,4 +0,0 @@
-// No idea what this is. Something to do with the serviceWorker.  But, what is the serviceWorker?
-console.info(
-  'Service worker disabled for development, will be generated at build time.'
-);
