85bfdc3f496fac641c916b2475857d2113978f87
vvasuki <vishvas.vasuki@gmail.com>
2019-05-17 10:01:53 -0700

    Content update.

---
+++ docs/polymer-app/build/github/src/dict-entry-link.js
@@ -0,0 +1,22 @@
+/*This element is used to present a given dictionary definition location nicely. Only difference from dict-entry-view is in the shadow-dom in the template.*/
+import { DictEntryView } from './dict-entry-view.js';
+import { html } from "../node_modules/@polymer/polymer/lib/utils/html-tag.js";
+
+class DictEntryLink extends DictEntryView {
+  static get template() {
+    return html`
+        <paper-card heading="">
+            <div class="entry-location">
+                [[locationSummary]]
+            </div>
+        </paper-card>
+`;
+  }
+
+  static get is() {
+    return 'dict-entry-link';
+  }
+
+}
+
+window.customElements.define(DictEntryLink.is, DictEntryLink);
---
+++ docs/polymer-app/build/github/src/dict-entry-view.js
@@ -0,0 +1,58 @@
+/*This element is used to present a given dictionary definition nicely.*/
+import { PolymerElement } from "../node_modules/@polymer/polymer/polymer-element.js";
+import "../node_modules/@polymer/paper-card/paper-card.js";
+import './shared-styles.js';
+import { html } from "../node_modules/@polymer/polymer/lib/utils/html-tag.js";
+export class DictEntryView extends PolymerElement {
+  static get template() {
+    return html`
+    <!--See - we extend shared-styles.-->
+    <style include="shared-styles">
+      :host {
+        display: block;
+
+        padding: 10px;
+      }
+    </style>
+
+    <paper-card heading="">
+        <div class="card-content">
+            [[locationSummary]]
+        </div>
+        <hr>
+        <div class="card-content">
+            <template is="dom-repeat" items="[[dictEntry.headwords]]">
+                [[item.text]]
+            </template>
+        </div>
+        <hr>
+        <div class="card-content">
+            [[dictEntry.entry.text]]
+        </div>
+    </paper-card>
+`;
+  }
+
+  static get is() {
+    return 'dict-entry-view';
+  }
+
+  static get properties() {
+    return {
+      dictEntry: Object,
+      // A human friendly summary of where this entry is from.
+      locationSummary: {
+        type: String,
+        computed: 'getLocationSummary(dictEntry.location)'
+      }
+    };
+  } // Used to compute locationSummary property based on changes to dictEntry property.
+
+
+  getLocationSummary(locationObj) {
+    var dictIdParts = locationObj.dictionaryId.split("__");
+    return dictIdParts[dictIdParts.length - 1].replace(/.babylon.*/, "") + `:${locationObj.entryNumber}`;
+  }
+
+}
+window.customElements.define(DictEntryView.is, DictEntryView);
